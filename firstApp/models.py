644f24386124c27edd5c8bbc23bd5b2fc3ace702
Angelo Correia <angelo.correia@devgo.org>
2023-08-28 16:46:01 -0100

    add module

--- firstApp/models.py
+++ firstApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Empoyee(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=20)
+    sal = models.DecimalField(max_digits=10, decimal_places=3)
+
+    def __str__(self) -> str:
+        return super().__str__()
